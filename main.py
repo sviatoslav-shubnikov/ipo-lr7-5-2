5c8c379b1b1e2dcb8132f03be39201341ae72092
sviatoslav-shubnikov <slavik20012008@gmail.com>
2024-12-12 14:31:48 +0300

    +

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ with open('dump.json', 'r', encoding='utf-8') as file:
 
 numb = input("Введите номер квалификации: ")
 
+found = False
+
 print("\n=============== Результат поиска ===============")
+
+specialty = 0
+
 for item in data:
-    if item['model'] == 'data.skill' and item['fields']['code'].startswith(numb):
-        print(f"{item['fields']['code']} >> Специальность {item['fields']['title']}")
+    if item['model'] == 'data.skill'  and item['fields']['code']==numb:
+
+        print(f"{item['fields']['code']} >> Квалификация {item['fields']['title']}")
+        specialty = item['fields']['specialty']
         found = True
+        
+for item in data:
+    if item['model'] == 'data.specialty':
+
+        specialty_pk = item['pk']
+
+        if specialty == specialty_pk:
+
+            print(f"{item['fields']['code']} >> Специальность {item['fields']['title']}")
 
 if not found:
     print("============== Не найдено ===============")
