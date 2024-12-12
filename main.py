0c46d0b6ca5a5f58ececf9f806c4caeb78596417
😈Sviatoslav🤑 <slavik20012008@gmail.com>
2024-12-12 21:15:01 +0300

    +

--- main.py
+++ main.py
@@ -3,30 +3,39 @@ import json
 with open('dump.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-
 numb = input("Введите номер квалификации: ")
 
 found = False
 
-print("\n=============== Результат поиска ===============")
-
-specialty = 0
-
 for item in data:
-    if item['model'] == 'data.skill'  and item['fields']['code']==numb:
+    if item['model'] == 'data.skill'  and item['fields']['code'] == numb:
 
-        print(f"{item['fields']['code']} >> Квалификация {item['fields']['title']}")
         specialty = item['fields']['specialty']
+
+        kval_code = item['fields']['code']
+        kval_title = item['fields']['title']
+
         found = True
-        
-for item in data:
-    if item['model'] == 'data.specialty':
 
-        specialty_pk = item['pk']
-
-        if specialty == specialty_pk:
-
-            print(f"{item['fields']['code']} >> Специальность {item['fields']['title']}")
 
 if not found:
     print("============== Не найдено ===============")
+
+
+else:
+
+    for item in data:
+
+        if item['model'] == 'data.specialty':
+
+            specialty_pk = item['pk']
+
+            if specialty == specialty_pk:
+
+                spec_code = item['fields']['code']
+                spec_title = item['fields']['title']
+                spec_educat = item['fields']['c_type']
+
+    print("============== Найдено ===============")
+    print(f"{spec_code} >> Специальность {spec_title}, {spec_educat}")
+    print(f"{kval_code} >> Квалификация {kval_title}")
